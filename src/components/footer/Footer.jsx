f1034088df788c21eced8202a90e860aa05a688e
Dev-Emmyy <emmanuelugochukwu2000@gmail.com>
2024-01-28 11:51:14 +0100

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div class="mt-6 bg-[#000000] text-[#f9f9f9] flex items-center justify-between gap-5 py-16 px-12 w-full">
             <div class="w-2/6">
-                <h2 class="font-semibold font-mono  italic">
+                <h2 class="font-semibold font-mono f italic">
                    Monique Coco Fashion Design leads the way so embrace elegance and define your essence.
                 </h2>
             </div>
@@ -18,7 +18,7 @@ const Footer = () => {
                     <p>+234 9034573549</p>
                     <p>No 5 Hd Plaza, Ahmadu Bello Way, Bauchi, Nigeria.</p>
                 </div>
-                <Link href="">
+                <Link href="https://l.instagram.com/?u=https%3A%2F%2Fmaps.app.goo.gl%2FMURYtBKeaHxx9fts5&e=AT1EiAkyysMB3rO_LqzWmNXJxhLY2dIn8V5s63UtGy1WLZghT4mfLecA6Di7U6SlkD8_fQbpfvvbe_ONcoS2fOf72_CmvGIpZWK0mw8" target="blank">
                     <p class="text-xl font-semibold underline">
                     SEE ON MAP
                     </p>
@@ -29,17 +29,17 @@ const Footer = () => {
                 <h2 class="text-xl font-semibold">FOLLOW US ON</h2>
                 <div class="flex flex-col content-center justify-between gap-5">
                     <Link href="">
-                        <p class="">
+                        <p class="underline">
                         Facebook
                         </p>
                     </Link>
-                    <Link href="">
-                        <p class="">
+                    <Link href="https://www.instagram.com/moniquecocochanel?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="blank">
+                        <p class="underline">
                         Instagram 
                         </p>
                     </Link>
                     <Link href="">
-                        <p class="">
+                        <p class="underline">
                         Tiktok
                         </p>
                     </Link>
