1571c1919d8c2e62b51b35b3d9d40867531f4a39
Dev-Emmyy <emmanuelugochukwu2000@gmail.com>
2024-03-11 00:19:34 +0100

    commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,56 +1,7 @@
 'use client';
-import { useState } from 'react';
-import { SMTPClient } from 'emailjs';
+import React from 'react';
 
 const Contact = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        // Initialize the SMTP client with your SMTP server details
-        const client = new SMTPClient({
-            user: 'user',
-            password: 'password',
-            host: 'smtp.your-email.com',
-            ssl: true,
-        });
-
-        // Construct the email message
-        const message = {
-            text: formData.message,
-            from: `${formData.name} <${formData.email}>`,
-            to: 'emmanuelugochukwu2000@gmail.com', // Specify your recipient's email address
-            subject: 'Contact Form Submission'
-        };
-
-        // Send the email
-        client.send(message, (err, response) => {
-            if (err) {
-                console.error('Error sending email:', err);
-            } else {
-                console.log('Email sent successfully:', response);
-                // Optionally, reset the form fields after successful submission
-                setFormData({
-                    name: '',
-                    email: '',
-                    message: ''
-                });
-            }
-        });
-    };
 
   return (
     <div>
@@ -61,7 +12,8 @@ const Contact = () => {
       </div>
 
       <div class="my-14 mx-12">
-        <form onSubmit={handleSubmit}>
+        <form id="fs-frm" name="simple-contact-form" acceptCharset="utf-8" action="https://formspree.io/f/xrgnlayp" method="post">
+        <fieldset id="fs-frm-inputs">
           <div class="mb-4">
             <label htmlFor="user_name" class="block text-[#013220] font-semibold">
               Full Name <a class="text-[#ff007f]">*</a>
@@ -70,8 +22,6 @@ const Contact = () => {
               type="text"
               id="name"
               name="name" 
-              value={formData.name} 
-              onChange={handleChange}
               class="border-solid border-2 border-black-500 rounded outline-none w-6/12 py-1 px-3 focus:border-rose-200 focus:border-4"
               required
             />
@@ -82,10 +32,8 @@ const Contact = () => {
             </label>
             <input
               type="email"
-              id="email" 
-              name="email" 
-              value={formData.email} 
-              onChange={handleChange}
+              name="_replyto" 
+              id="email-address"
               class="border-solid border-2 border-black-500 rounded outline-none w-6/12 py-1 px-3 focus:border-rose-200 focus:border-4"
               required
             />
@@ -95,15 +43,15 @@ const Contact = () => {
               How may we help you? <a class="text-[#ff007f]">*</a>
             </label>
             <textarea
-             id="message" 
-             name="message" 
-             value={formData.message} 
-             onChange={handleChange}
+              id="message" 
+              name="message" 
               class="border-solid border-2 border-black-500 rounded outline-none w-6/12 h-60 py-1 px-3 focus:border-rose-200 focus:border-4"
               required
             />
           </div>
-          <input type="submit" value="Send" class="bg-[#013220] text-[#FFFDD0] px-6 py-2 transition-all duration-300 border hover:text-black hover:border-black hover:bg-[#f9f9f9]" />
+          <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
+          </fieldset>
+          <input type="submit" value="Send" class="bg-[#013220] text-[#FFFDD0] px-6 py-2 transition-all duration-300 border hover:text-black hover:border-black hover:bg-[#f9f9f9] hover:cursor-pointer" />
         </form>
       </div>
     </div>
